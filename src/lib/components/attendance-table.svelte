3a524508e5ef46dc2ca72b878c216f6e7c61a904
Juan Miguel Villegas <70586029+unovil@users.noreply.github.com>
2026-02-19 19:03:07 +0800

    deleted all work files to restart project

--- src/lib/components/attendance-table.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import * as Table from '$lib/components/ui/table';
-
-	let {
-		studentAttendances = $bindable()
-	}: {
-		studentAttendances: string;
-	} = $props();
-</script>
